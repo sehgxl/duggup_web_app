06ef48dac80fc61717c25f52826d765e593d829d
sehgxl <abhinavsehgal802@gmail.com>
2024-03-12 17:59:51 +0530

    feat: add nav button component

---
+++ src/lib/components/NavButton.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	export let buttonText: string;
+	export let navLink: string;
+	export let iconUrl: string;
+</script>
+
+<a href={navLink}>
+	<Button variant="link" class="bg- bg-whit flex items-center gap-2 p-1">
+		<img class="m-1 h-4 w-4" src={iconUrl} alt="HomeIcon" />
+		<h1 class="text-lightBlack text-xs font-normal leading-5">{buttonText}</h1>
+	</Button>
+</a>
